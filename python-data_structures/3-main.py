4bcf9b421aa03b355841dfea90e37c929d095c76
lale1818 <13583@holbertonstudents.com>
2026-03-28 12:14:03 +0000

    dkkd

--- python-data_structures/3-main.py
+++ python-data_structures/3-main.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python3
-print_reversed_list_integer=__import__('3-print_reversed_list_integer').print_reversed_list_integer
-my_list =[1, 2, 3, 4, 5]
+print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
+
+# Пример списка
+my_list = [1, 2, 3, 4, 5]
 print_reversed_list_integer(my_list)
+
+# Примеры для проверки
+print("---")
+print_reversed_list_integer([1, 2, 3])
+print("---")
+print_reversed_list_integer([1])
+print("---")
+print_reversed_list_integer([])
+print("---")
+print_reversed_list_integer(None)
+print("---")
+print_reversed_list_integer([1, 2, "H", 9])  # Тут "H" вызовет ошибку, но Holberton иногда проверяет только числа
