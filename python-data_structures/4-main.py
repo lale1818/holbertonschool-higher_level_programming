2308bc71e22e2ab858337076d490eb6dcb9cfc0c
lale1818 <13583@holbertonstudents.com>
2026-03-28 12:54:05 +0000

    mkkfkf

--- python-data_structures/4-main.py
+++ python-data_structures/4-main.py
@@ -6,5 +6,5 @@ idx = 3
 new_element = 9
 new_list = new_in_list(my_list, idx, new_element)
 
-print(new_list)  # [1, 2, 3, 9, 5]
-print(my_list)   # [1, 2, 3, 4, 5] — оригинал не изменё
+print(new_list)
+print(my_list)
--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    """Return a new list with element replaced at idx without modifying original list."""
-    # создаём копию списка
     new_list = my_list[:]
-    # проверяем, что индекс корректный
     if 0 <= idx < len(new_list):
         new_list[idx] = element
-    # возвращаем новый список
     return new_list
