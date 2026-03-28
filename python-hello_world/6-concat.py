a65cf517346791bc56a6619d2130c58698fb7da6
lale1818 <13583@holbertonstudents.com>
2026-03-28 19:37:50 +0000

    Fix 6-concat.py with comments and variables

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-str1 = "Welcome to "
-str2 = "Holberton School!"
+str1 = "Welcome to "  # first part of the string
+str2 = "Holberton School!"  # second part of the string
 print(str1 + str2)
