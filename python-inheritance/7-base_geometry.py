089e21d7b103f512052967d4fd25076284810ee1
lale1818 <13583@holbertonstudents.com>
2026-04-11 19:35:41 +0000

    Add integer_validator to BaseGeometry

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -8,15 +8,20 @@ class BaseGeometry:
     """Həndəsi fiqurlar üçün baza klası."""
 
     def area(self):
-        """Sahəni hesablayan metod."""
+        """Sahəni hesablayan metod (hələ tətbiq olunmayıb)."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """Dəyərin tam ədəd və müsbət olduğunu yoxlayır.
+
+        Args:
+            name (str): Parametrin adı.
+            value (int): Yoxlanılacaq dəyər.
+
+        Raises:
+            TypeError: Əgər value integer deyilsə.
+            ValueError: Əgər value <= 0 olarsa.
         """
-        Dəyərin mütləq tam ədəd (və Boolean olmayan) 
-        və müsbət olduğunu yoxlayır.
-        """
-        # Diqqət: type(value) is int - Boolean-ları (True/False) bloklayır
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
