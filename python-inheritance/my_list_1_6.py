7fb2a48d4c558e0b835785073e9dcb03596ec24d
lale1818 <13583@holbertonstudents.com>
2026-04-11 08:28:16 +0000

    Fix MyList print_sorted

--- python-inheritance/my_list_1_6.py
+++ python-inheritance/my_list_1_6.py
@@ -6,7 +6,5 @@ class MyList(list):
     """Class MyList that inherits from list"""
 
     def print_sorted(self):
-        """Print list in ascending order and return a new sorted list"""
-        new_list = sorted(self)
-        print(new_list)
-        return new_list
+        """Print list in ascending order without modifying it"""
+        print(sorted(self))
