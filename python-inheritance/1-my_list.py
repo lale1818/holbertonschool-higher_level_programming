cd887cca17b16ee2c4afdae38380a33a266429fc
lale1818 <13583@holbertonstudents.com>
2026-04-11 08:53:08 +0000

    Fix MyList

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
 """Module MyList"""
-class MyList(list):
-    """Class that inherits from list"""'
-    def print_sorted(self):
-        """Print list in ascending sorted order"""
-        print(sorted(self))
-#!/usr/bin/python3
-"""Module MyList"""
 
 
 class MyList(list):
