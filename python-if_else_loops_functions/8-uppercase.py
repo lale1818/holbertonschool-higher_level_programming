6115d1af0632497a6e0ddba77c2872c20f7a78e5
lale1818 <13583@holbertonstudents.com>
2026-06-08 20:46:18 +0000

    Fix task 8: Optimize to a single print function to satisfy checker constraint

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
     """Prints a string in uppercase followed by a new line"""
+    result = ""
     for c in str:
         if ord(c) >= 97 and ord(c) <= 122:
-            print("{}".format(chr(ord(c) - 32)), end="")
+            result += chr(ord(c) - 32)
         else:
-            print("{}".format(c), end="")
-    print("")
+            result += c
+    print("{}".format(result))
