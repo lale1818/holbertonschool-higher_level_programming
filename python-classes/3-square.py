0d916e4f5f82b1e25e85afbd5e8812aee1736fd4
lale1818 <13583@holbertonstudents.com>
2026-06-08 20:55:56 +0000

    Add task 3: Implement public area() method in Square class

---
+++ python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """Represents a square with area calculation capability"""
+
+    def __init__(self, size=0):
+        """Initializes the square and validates the size"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Calculates and returns the current square area"""
+        return self.__size ** 2
