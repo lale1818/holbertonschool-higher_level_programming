2a5e04324b3885875c089c9e255de74a79cdd426
lale1818 <13583@holbertonstudents.com>
2026-06-08 20:26:43 +0000

    Add task 4: String concatenation

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1 = "Welcome to "  # first part
-str2 = "Holberton School!"  # second part
-# Printing the full message
-print(str1 + str2)
+str1 = "Holberton"
+str2 = "School"
+str1 = "Welcome to " + str1 + " " + str2 + "!"
+print(str1)
